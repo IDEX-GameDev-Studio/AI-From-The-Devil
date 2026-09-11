1e5b38c1a2e81f84f95ce9035371e85d49c2a706
MRMIL1234 <mihajlolavrov01@gmail.com>
2026-09-11 21:02:52 +0300

    [WIP] feat: continue playerInteraction

--- AI-FromTheDevil/Assets/Scripts/Interaction/PlayerInteraction.cs
+++ AI-FromTheDevil/Assets/Scripts/Interaction/PlayerInteraction.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    [SerializeField] private float _rayDisctance;
+    private PlayerInputActions _playerInputActions;
+    [SerializeField] private float _rayDistance;
 
     private Camera _camera;
+
+    private void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+    }
+    private void OnEnable()
+    {
+        _playerInputActions.Enable();
+    }
+    private void OnDisable()
+    {
+        _playerInputActions.Disable();
+    }
     void Start()
     {
         _camera = Camera.main;
@@ -13,11 +28,14 @@ public class PlayerInteraction : MonoBehaviour
     {
         Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, _rayDisctance))
+        if (Physics.Raycast(ray, out hit, _rayDistance))
         {
-            if (hit.collider.TryGetComponent(out IInteractable interactable) && ))
-            {))
-        }
+            if (hit.collider.TryGetComponent(out IInteractable interactable) )
+            {
+                return;
+            }
 
+        }
     }
+    
 }
